bf3ed2ec6108267f9651b29df7cdd4b603bd047d
mdumair999 <mdumairahsan@gmail.com>
2024-03-24 13:58:07 +0300

    addition programm

---
+++ addition.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Perform addition
+result = add_numbers(num1, num2)
+
+# Display the result
+print("The sum of", num1, "and", num2, "is:", result)
